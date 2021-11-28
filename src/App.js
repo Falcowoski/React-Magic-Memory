d1419a43349f987656e53ce3835a09c972ac7382
Leonardo Falcoski <leonardo.falcoski@gmail.com>
2021-11-28 01:47:40 -0300

    Alterado caminho das imagens e adicionado GitHub Pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/helmet-1.png", matched: false },
-  { "src": "/img/potion-1.png", matched: false },
-  { "src": "/img/ring-1.png", matched: false },
-  { "src": "/img/scroll-1.png", matched: false },
-  { "src": "/img/shield-1.png", matched: false },
-  { "src": "/img/sword-1.png", matched: false }
+  { "src": "./img/helmet-1.png", matched: false },
+  { "src": "./img/potion-1.png", matched: false },
+  { "src": "./img/ring-1.png", matched: false },
+  { "src": "./img/scroll-1.png", matched: false },
+  { "src": "./img/shield-1.png", matched: false },
+  { "src": "./img/sword-1.png", matched: false }
 ]
 
 function App() {
